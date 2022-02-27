d846b3bd9e997ab74c2c05c10cf0942bc70e8c35
darkseear <38041141+darkseear@users.noreply.github.com>
2022-02-28 01:46:00 +0300

    public

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,21 @@ import dotenv from "dotenv"
 dotenv.config();
 const app = express();
 const port = process.env.PORT;
-app.get('/', (request, response)=>{
+app.get('/appget/', (request, response)=>{
     response.send('hello');
 });
+
+
+app.use(express.static("public"));
+// app.use(bodyParser.json());
+// app.use(cors());
+// app.use(function (req, res, next) {
+//     // console.log(req.header('Origin'));
+//     res.setHeader('Access-Control-Allow-Origin', "*");
+//     res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+//     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers');
+//     next();
+// });
+// app.express.use(bodyParser.urlencoded({ extended: true }))
 
 app.listen(port, () => console.log(`Running on port ${port}`));
