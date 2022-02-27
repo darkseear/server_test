0823092d96bdbd3afdd8c0a33e1c98ab453f9ec4
darkseear <38041141+darkseear@users.noreply.github.com>
2022-02-28 02:23:46 +0300

    new file

--- dist/index.js
+++ dist/index.js
@@ -4,10 +4,23 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 exports.__esModule = true;
 var express_1 = __importDefault(require("express"));
+var dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1["default"].config();
 var app = (0, express_1["default"])();
-var port = 5000;
-app.get('/', function (request, response) {
+var port = process.env.PORT;
+app.get('/appget/', function (request, response) {
     response.send('hello');
 });
+app.use(express_1["default"].static("public"));
+// app.use(bodyParser.json());
+// app.use(cors());
+// app.use(function (req, res, next) {
+//     // console.log(req.header('Origin'));
+//     res.setHeader('Access-Control-Allow-Origin', "*");
+//     res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+//     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers');
+//     next();
+// });
+// app.express.use(bodyParser.urlencoded({ extended: true }))
 app.listen(port, function () { return console.log("Running on port ".concat(port)); });
 //# sourceMappingURL=index.js.map
