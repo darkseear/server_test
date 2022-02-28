c60f7d2bbb6268b26cc2278188eb5aef3013e0a3
darkseear <darkseeara@yandex.ru>
2022-02-28 17:06:58 +0300

    Добавление log4js и eslint

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,22 @@
-import express from "express"
-import dotenv from "dotenv"
+import express from 'express';
+import dotenv from 'dotenv';
+import log4js from 'log4js';
 
 dotenv.config();
+
+const logger = log4js.getLogger();
+logger.level = process.env.LOG_LEVEL;
+
+logger.info('log4js log info');
+logger.debug('log4js log debug');
+logger.error('log4js log error');
+
 const app = express();
 const port = process.env.PORT;
-app.get('/appget/', (request, response)=>{
-    response.send('hello');
+app.get('/appget/', (request, response) => {
+  response.send('hello');
 });
 
-
-app.use(express.static("public"));
-// app.use(bodyParser.json());
-// app.use(cors());
-// app.use(function (req, res, next) {
-//     // console.log(req.header('Origin'));
-//     res.setHeader('Access-Control-Allow-Origin', "*");
-//     res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
-//     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers');
-//     next();
-// });
-// app.express.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static('public'));
 
 app.listen(port, () => console.log(`Running on port ${port}`));
