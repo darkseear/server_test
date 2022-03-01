033ab80e88f1308529379f7c1a1cf8ff6985ce1a
darkseear <38041141+darkseear@users.noreply.github.com>
2022-03-02 01:23:25 +0300

    build from client

--- dist/index.js
+++ dist/index.js
@@ -5,22 +5,18 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 exports.__esModule = true;
 var express_1 = __importDefault(require("express"));
 var dotenv_1 = __importDefault(require("dotenv"));
+var log4js_1 = __importDefault(require("log4js"));
 dotenv_1["default"].config();
+var logger = log4js_1["default"].getLogger();
+logger.level = process.env.LOG_LEVEL;
+logger.info('log4js log info');
+logger.debug('log4js log debug');
+logger.error('log4js log error');
 var app = (0, express_1["default"])();
 var port = process.env.PORT;
 app.get('/appget/', function (request, response) {
     response.send('hello');
 });
-app.use(express_1["default"].static("public"));
-// app.use(bodyParser.json());
-// app.use(cors());
-// app.use(function (req, res, next) {
-//     // console.log(req.header('Origin'));
-//     res.setHeader('Access-Control-Allow-Origin', "*");
-//     res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
-//     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers');
-//     next();
-// });
-// app.express.use(bodyParser.urlencoded({ extended: true }))
+app.use(express_1["default"].static('../typescript_for_test2/build'));
 app.listen(port, function () { return console.log("Running on port ".concat(port)); });
 //# sourceMappingURL=index.js.map
